f09b604b016b0bc61db768cd621bbf2f8086606a
Fischaela <michaela.lehr@geildanke.com>
2018-04-08 11:13:01 +0200

    MOD remove unused parameter, button size

--- components/photo_gallery_button.js
+++ components/photo_gallery_button.js
@@ -2,7 +2,7 @@ AFRAME.registerComponent('photo-gallery-button', {
   init: function () {
     this.el.addEventListener('click', this.onClick.bind(this));
   },
-  onClick: function (event) {
+  onClick: function () {
     var id = '#' + this.el.getAttribute('data-image');
     document.querySelector('#panorama').setAttribute('material', 'src', id);
   }
